8eba1c4dfa6ef144b3556b81bce94eecce4ea0e4
TRIDIBPAUL17 <137639265+TRIDIBPAUL17@users.noreply.github.com>
2025-04-19 20:29:23 +0530

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,32 +1,39 @@
 import React from 'react'
 import AccountIcon from './AccountIcon'
 import CompareButton from './CompareButton'
-import LOGO from '../pastille-methode.svg'
 
 const Header = () => {
   return (
-    <div className="header" style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', padding: '10px 20px' }}>
-      
+    <div
+      className="header"
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        padding: '10px 20px',
+      }}
+    >
       <div style={{ display: 'flex', alignItems: 'center' }}>
-        <img
-          src="/favi.png"
-          alt="Logo"
-          style={{
-            height: '50px',
-            width: '200px',
-            borderRadius: '10%',
-            objectFit: 'cover',
-            marginRight: '10px'
-          }}
-        />
+        <a href="/" style={{ display: 'flex', alignItems: 'center' }}>
+          <img
+            src="/favi.png"
+            alt="Logo"
+            style={{
+              height: '50px',
+              width: '200px',
+              borderRadius: '10%',
+              objectFit: 'cover',
+              marginRight: '10px',
+              cursor: 'pointer',
+            }}
+          />
+        </a>
         <CompareButton />
       </div>
-
 
       <div>
         <AccountIcon />
       </div>
-      
     </div>
   )
 }
